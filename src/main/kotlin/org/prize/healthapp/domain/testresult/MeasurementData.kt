a0e8936ef04a7a29efaa5661bf4675374c7d1375
Ewan Jee <830jiforcareer@gmail.com>
2024-11-05 15:53:12 -0500

    feat : CSVRecord 필요 없음으로 패러미터 값 변경

--- src/main/kotlin/org/prize/healthapp/domain/testresult/MeasurementData.kt
+++ src/main/kotlin/org/prize/healthapp/domain/testresult/MeasurementData.kt
@@ -1,7 +1,6 @@
 package org.prize.healthapp.domain.testresult
 
 import kotlinx.serialization.Serializable
-import org.apache.commons.csv.CSVRecord
 
 @Serializable
 data class MeasurementData(
@@ -22,23 +21,23 @@ data class MeasurementData(
     val fiveMx4ShuttleRunSeconds: Double = 0.0,
 ) {
     companion object {
-        fun from(columns: CSVRecord): MeasurementData =
+        fun from(column: Map<String, String>): MeasurementData =
             MeasurementData(
-                columns[9].toDouble(),
-                columns[10].toDouble(),
-                columns[11].toDouble(),
-                columns[18].toInt(),
-                columns[25].toDouble(),
-                columns[26].toInt(),
-                columns[27].toInt(),
-                columns[28].toDouble(),
-                columns[29].toInt(),
-                columns[30].toInt(),
-                columns[32].toInt(),
-                columns[41].toInt(),
-                columns[45].toDouble(),
-                columns[46].toDouble(),
-                columns[52].toDouble(),
+                height = column["MESURE_IEM_001_VALUE"]?.toDoubleOrNull() ?: 0.0,
+                weight = column["MESURE_IEM_002_VALUE"]?.toDoubleOrNull() ?: 0.0,
+                bodyFatPercentage = column["MESURE_IEM_003_VALUE"]?.toDoubleOrNull() ?: 0.0,
+                sitUpCount = column["MESURE_IEM_010_VALUE"]?.toIntOrNull() ?: 0,
+                bmiKgPerM2 = column["MESURE_IEM_018_VALUE"]?.toDoubleOrNull() ?: 0.0,
+                crossedSitUpCount = column["MESURE_IEM_019_VALUE"]?.toIntOrNull() ?: 0,
+                shuttleRunCount = column["MESURE_IEM_020_VALUE"]?.toIntOrNull() ?: 0,
+                tenMx4ShuttleRunSeconds = column["MESURE_IEM_021_VALUE"]?.toDoubleOrNull() ?: 0.0,
+                standingLongJumpCm = column["STANDING_LONG_JUMP_CM"]?.toIntOrNull() ?: 0,
+                sitToStandCount = column["MESURE_IEM_022_VALUE"]?.toIntOrNull() ?: 0,
+                twoMinuteSteppingInPlaceCount = column["MESURE_IEM_025_VALUE"]?.toIntOrNull() ?: 0,
+                treadmill9MinutesBpm = column["MESURE_IEM_034_VALUE"]?.toIntOrNull() ?: 0,
+                thighCircumferenceLeftCm = column["MESURE_IEM_038_VALUE"]?.toDoubleOrNull() ?: 0.0,
+                thighCircumferenceRightCm = column["MESURE_IEM_039_VALUE"]?.toDoubleOrNull() ?: 0.0,
+                fiveMx4ShuttleRunSeconds = column["MESURE_IEM_050_VALUE"]?.toDoubleOrNull() ?: 0.0,
             )
     }
 }
