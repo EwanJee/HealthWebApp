07a5c2b61dae2a2e1f52b13c161a7c54386c49b5
Ewan Jee <830jiforcareer@gmail.com>
2024-11-06 19:22:25 -0500

    feat : ErrorCode WrongFIleName MissingColumn 에러코드 추가

--- src/main/kotlin/org/prize/healthapp/domain/exception/ErrorCode.kt
+++ src/main/kotlin/org/prize/healthapp/domain/exception/ErrorCode.kt
@@ -15,4 +15,10 @@ enum class ErrorCode(
 
     // TOO MANY REQUEST
     TOO_MANY_REQUEST(HttpStatus.TOO_MANY_REQUESTS, "F003", "요청이 너무 많습니다."),
+
+    // WRONG FILE NAME
+    WRONG_FILE_NAME(HttpStatus.BAD_REQUEST, "F004", "잘못된 파일 이름입니다."),
+
+    // MISSING COLUMN
+    MISSING_COLUMN(HttpStatus.BAD_REQUEST, "F005", "필수 칼럼이 누락되었습니다."),
 }
